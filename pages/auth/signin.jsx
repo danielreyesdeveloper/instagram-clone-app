fd5495061818357f2bd84be9ecbbb5a6c35f1cf6
unknown <danielreyesdev@gmail.com>
2022-03-27 19:23:04 -0600

    noreferer added to anchor

--- pages/auth/signin.jsx
+++ pages/auth/signin.jsx
@@ -7,7 +7,7 @@ export default function signin({providers}) {
       <Header/>
       <div className="flex flex-col items-center justify-center min-h-screen py-2 -mt-56 px-14 text-center">
         <img className="w-80 mb-2" src="https://links.papareact.com/ocw" alt="" />
-        <p>Instagram clone made with HTML, CSS, JavaScript, Tailwind, React, Next, Recoil and Firebase by <a className="text-blue-500 hover:text-blue-700" href="https://www.linkedin.com/in/daniel-reyes-developer/" target="_blank">Daniel Reyes</a> </p>
+        <p>Instagram clone made with HTML, CSS, JavaScript, Tailwind, React, Next, Recoil and Firebase by <a className="text-blue-500 hover:text-blue-700" href="https://www.linkedin.com/in/daniel-reyes-developer/" target="_blank" rel="noreferrer">Daniel Reyes</a> </p>
         <div className="mt-20">
           {Object.values(providers).map((provider) => (
             <div key={provider.name}>
