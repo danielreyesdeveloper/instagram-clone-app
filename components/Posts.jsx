8fae7232a07bd934e76a00aeb657c4b377e00c5c
unknown <danielreyesdev@gmail.com>
2022-03-27 19:14:34 -0600

    likes ui modified

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,5 +1,5 @@
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore"
-import { db, storage } from "../firebase"
+import { db } from "../firebase"
 import { useState, useEffect } from 'react'
 import Post from './Post'
 
